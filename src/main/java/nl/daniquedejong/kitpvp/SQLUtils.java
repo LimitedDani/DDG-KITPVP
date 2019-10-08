a7c79f6739075338346734b3de731e662bf307b0
Daníque de Jong <danique.de.jong2000@gmail.com>
2019-10-08 21:21:52 +0200

    Added documentation to the functions

--- src/main/java/nl/daniquedejong/kitpvp/SQLUtils.java
+++ src/main/java/nl/daniquedejong/kitpvp/SQLUtils.java
@@ -7,6 +7,10 @@ import java.sql.Statement;
 import java.util.UUID;
 
 public class SQLUtils {
+    /**
+     * @param plugin Main class
+     * @param uuid Unique ID of the player
+     */
     public static void createPlayerRowIfNotExist(Plugin plugin, UUID uuid) throws SQLException {
         String query = "INSERT INTO ddg.player_stats (UUID) " +
                 "SELECT * FROM (SELECT '" + uuid.toString() + "') AS tmp " +
@@ -17,12 +21,25 @@ public class SQLUtils {
         preparedStatement.execute();
         preparedStatement.close();
     }
+
+    /**
+     * @param plugin Main class
+     * @param uuid Unique ID of the player
+     * @return ResultSet
+     */
     public static ResultSet getKillsAndDeaths(Plugin plugin, UUID uuid) throws SQLException {
         String query = "SELECT * FROM ddg.player_stats WHERE uuid='" + uuid.toString()  + "'";
         Statement st = plugin.mysql.getConnection().createStatement();
         ResultSet rs = st.executeQuery(query);
         return rs;
     }
+
+    /**
+     * @param plugin Main class
+     * @param uuid Unique ID of the player
+     * @param kills amount of kills the player has
+     * @param deaths amount of kills the player has
+     */
     public static void updateKillsAndDeaths(Plugin plugin, UUID uuid, int kills, int deaths) throws SQLException {
         String query = "UPDATE ddg.player_stats SET kills = " + kills + ", deaths = " + deaths + " WHERE uuid='" + uuid.toString()  + "'";
         PreparedStatement prepareStatement = plugin.mysql.getConnection().prepareStatement(query);
